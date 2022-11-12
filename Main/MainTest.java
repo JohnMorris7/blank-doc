86aaf987712e6313f3875f96ee5470fed5cc2c69
John <john@Zipcoders-MacBook-Pro-46.local>
2022-11-12 12:53:08 -0500

    creating tests

---
+++ Main/MainTest.java
@@ -0,0 +1,18 @@
+package Main;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void test(){
+        //Given: an array of numbersw as strings,
+        //i,e,, {"1", "5", "15", "9", "4"}"
+        //Task: sort the numbers
+        //Restriction: the numbers must stay strings - no conberting to integers
+
+
+    }
+
+}
